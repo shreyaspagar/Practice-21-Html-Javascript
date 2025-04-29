7d7154082ae1d1efaf80e8326504690473426dc1
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-04-29 19:34:15 +0530

    Add files via upload

---
+++ app46.js
@@ -0,0 +1,18 @@
+let number = 287152;
+let sum = 0;
+
+let copy = number;
+
+while (copy > 0) {
+    digit = copy % 10;
+    sum+= digit;
+    copy=Math.floor(copy / 10);
+
+}
+
+console.log(sum);
+
+
+
+
+
